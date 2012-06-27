bb6b0d26bfe1451b9b0db555e9b947e60107864b
Joel Junström <joel.junstrom@oktavilla.se>
2012-06-28 00:57:09 +0200

    Added url generator

---
+++ lib/doc_juans_helper/url_generator.rb
@@ -0,0 +1,60 @@
+require 'openssl'
+require 'cgi'
+
+module DocJuansHelper
+  class NoHostGivenError < StandardError; end
+  class NoSecretGivenError < StandardError; end
+
+  class UrlGenerator
+    attr_reader :url, :filename, :options
+
+    def initialize url, filename, options = {}
+      @url = url
+      @filename = filename.to_s
+      @options = Hash[(options || {}).sort]
+
+      raise NoSecretGivenError if secret_key == ''
+      raise NoHostGivenError if host == ''
+    end
+
+    def generate
+      params = []
+      params << "url=#{CGI.escape(url)}"
+      params << "filename=#{CGI.escape(filename)}"
+      options.each do |k,v|
+        params << "options[#{CGI.escape(k.to_s)}]=#{CGI.escape v}"
+      end
+      params << "key=#{public_key}"
+
+      "#{host}?#{params.join('&')}"
+    end
+
+    def public_key
+      sha1 = OpenSSL::Digest::Digest.new 'sha1'
+      OpenSSL::HMAC.hexdigest sha1, secret_key, seed_string
+    end
+
+    def seed_string
+      seed = []
+      seed << "filename:#{filename}"
+      options.each do |k,v|
+        seed << "options_#{k}:#{v}"
+      end
+      seed << "url:#{url}"
+
+      seed.join '-'
+    end
+
+    def host
+      @host ||= DocJuansHelper.config.host.to_s.strip.tap do |host|
+        if host != '' && ! host.start_with?('http')
+          host.replace "http://#{host}"
+        end
+      end
+    end
+
+    def secret_key
+      DocJuansHelper.config.secret.to_s.strip
+    end
+  end
+end
---
+++ spec/url_generator_spec.rb
@@ -0,0 +1,73 @@
+require_relative 'spec_helper'
+
+require_relative '../lib/doc_juans_helper/config.rb'
+require_relative '../lib/doc_juans_helper/url_generator.rb'
+
+describe DocJuansHelper::UrlGenerator do
+  before :each do
+    DocJuansHelper.config.host = 'doc-juan.example.com'
+    DocJuansHelper.config.secret = 'zecret'
+  end
+
+  subject do
+    DocJuansHelper::UrlGenerator.new('http://example.com', 'file.pdf', {
+      title: 'The Site',
+      size: 'A4'
+    })
+  end
+
+  it 'generates the url' do
+    url = subject.generate
+
+    expected = 'http://doc-juan.example.com?'
+    expected << "url=#{CGI.escape subject.url}"
+    expected << "&filename=#{CGI.escape subject.filename}"
+    expected << "&options[size]=A4"
+    expected << "&options[title]=#{CGI.escape 'The Site'}"
+    expected << "&key=#{subject.public_key}"
+
+    url.must_equal expected
+  end
+
+  it 'has the host' do
+    DocJuansHelper.config.host = 'example.com'
+
+    subject.host.must_equal 'http://example.com'
+  end
+
+  it 'fails with NoHostGivenError unless there is a host set' do
+    DocJuansHelper.config.host = nil
+
+    proc {
+      subject
+    }.must_raise DocJuansHelper::NoHostGivenError
+  end
+
+  it 'has the secret key' do
+    subject.secret_key.must_equal 'zecret'
+  end
+
+  it 'fails with NoSecretGivenError unless there is a secret set' do
+    DocJuansHelper.config.secret = nil
+
+    proc {
+      subject
+    }.must_raise DocJuansHelper::NoSecretGivenError
+  end
+
+  it 'compiles into a seed string for the public key computation' do
+    subject.seed_string.must_equal 'filename:file.pdf-options_size:A4-options_title:The Site-url:http://example.com'
+  end
+
+  it 'calculates the public key' do
+    subject.public_key.must_equal 'df9515da45dd0cd445a678928bc3e575cc106793'
+  end
+
+  it 'calculates the public key with no options given' do
+    url_generator = DocJuansHelper::UrlGenerator.new 'http://example.com', 'file.pdf'
+    key = url_generator.public_key
+
+    key.must_equal '539ebb1f6cd3fec40591acdc756e9b047e7093b3'
+  end
+
+end
